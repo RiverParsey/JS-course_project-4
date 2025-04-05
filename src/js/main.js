2cfcdfbe506179fbf4bee66dde1c25c280278342
ricsi <ricsi@ricsis-MacBook-Pro>
2025-04-05 19:55:50 +0200

    lesson-10_dynamic_creation_of_modals

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,39 @@ $('button').eq(2).on('click', () => {
   $('.w-500').fadeToggle(800);
 });
 
+$('#trigger').click(() => $('#trigger').createModal({
+  text: {
+    title: 'Modal title',
+    body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, ex. Reprehenderit aliquid nostrum impedit rem illo nam unde, minima, assumenda esse dignissimos quae omnis? Commodi enim harum eaque minima autem?'
+  },
+  btns: {
+    count: 3,
+    settings: [
+      [
+        'Close',
+        ['btn-danger', 'mr-10'],
+        true
+      ],
+      [
+        'Save changes',
+        ['btn-success'],
+        false,
+        () => {
+          alert('data saved')
+        }
+      ],
+      [
+        'Another button',
+        ['btn-warning', 'ml-10'],
+        false,
+        () => {
+          alert('hello world');
+        }
+      ]
+    ]
+  }
+}));
+
 // $('.wrap').html(
 //   `
 //   <div class="dropdown">
