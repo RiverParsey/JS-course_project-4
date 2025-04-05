6e8bc6ebbdbd00d7f3b34e09e0732b84ef2bb224
ricsi <ricsi@ricsis-MacBook-Pro>
2025-04-05 17:39:31 +0200

    lesson-8_create_dropdown_menu

--- dist/script.js
+++ dist/script.js
@@ -2,6 +2,27 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/js/lib/components/dropdown.js":
+/*!*******************************************!*\
+  !*** ./src/js/lib/components/dropdown.js ***!
+  \*******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../core */ "./src/js/lib/core.js");
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.dropdown = function () {
+  for (let i = 0; i < this.length; i++) {
+    const id = (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i]).getAtt('id');
+    (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i]).click(() => {
+      (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(`[data-toggle-id="${id}"]`).fadeToggle(300);
+    });
+  }
+};
+(0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.dropdown-toggle').dropdown();
+
+/***/ }),
+
 /***/ "./src/js/lib/core.js":
 /*!****************************!*\
   !*** ./src/js/lib/core.js ***!
@@ -51,6 +72,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _modules_attributes__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./modules/attributes */ "./src/js/lib/modules/attributes.js");
 /* harmony import */ var _modules_actions__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./modules/actions */ "./src/js/lib/modules/actions.js");
 /* harmony import */ var _modules_effects__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./modules/effects */ "./src/js/lib/modules/effects.js");
+/* harmony import */ var _components_dropdown__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./components/dropdown */ "./src/js/lib/components/dropdown.js");
+
 
 
 
@@ -167,6 +190,19 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.siblings = function () {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../core */ "./src/js/lib/core.js");
 
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.getAtt = function (attributeName) {
+  if (this.length > 0) {
+    return this[0].getAttribute(attributeName);
+  }
+  return null;
+};
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.getAllAtt = function (attributeName) {
+  let result = {};
+  for (let i = 0; i < this.length; i++) {
+    result[i] = this[i].getAttribute(attributeName);
+  }
+  return result;
+};
 _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.setAtt = function (attributeName, value) {
   for (let i = 0; i < this.length; i++) {
     this[i].setAttribute(attributeName, value);
@@ -446,6 +482,21 @@ __webpack_require__.r(__webpack_exports__);
 (0,_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('button').eq(2).on('click', () => {
   (0,_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('.w-500').fadeToggle(800);
 });
+
+// $('.wrap').html(
+//   `
+//   <div class="dropdown">
+//     <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">Dropdown button</button>
+//     <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
+//         <a href="#" class="dropdown-item">Action</a>
+//         <a href="#" class="dropdown-item">Action #2</a>
+//         <a href="#" class="dropdown-item">Action #3</a>
+//     </div>
+//   </div>
+//   `
+// );
+
+// $('.dropdown-toggle').dropdown();
 })();
 
 /******/ })()
--- src/js/lib/modules/attributes.js
+++ src/js/lib/modules/attributes.js
@@ -1,5 +1,22 @@
 import $ from '../core';
 
+$.prototype.getAtt = function(attributeName) {
+  if (this.length > 0) {
+    return this[0].getAttribute(attributeName);
+  }
+  
+  return null;
+}
+
+$.prototype.getAllAtt = function(attributeName) {
+  let result = {};
+  for (let i = 0; i < this.length; i++) {
+    result[i] = this[i].getAttribute(attributeName);
+  }
+
+  return result;
+}
+
 $.prototype.setAtt = function(attributeName, value) {
   for (let i = 0; i < this.length; i++) {
     this[i].setAttribute(attributeName, value);
